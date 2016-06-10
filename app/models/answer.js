d07b6058bdc6221e27a8abb56510bd7841d2312b
alexhagge128 <alexagge128@gmail.com>
2016-06-10 12:53:21 -0700

    implemented dynamic segmenting and model relationship

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,8 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
-  query: DS.attr(),
-  note: DS.attr(),
+  message: DS.attr(),
 
-  answers: DS.hasMany('answer', {async: true})
+  question: DS.belongsTo('question', {async: true})
 });
