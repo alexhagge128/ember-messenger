59f8ecd878d397bb7b7c020b1b135cb35e8d4419
alexhagge128 <alexhagge128@gmail.com>
2016-06-17 09:13:09 -0700

    implemented computed property with moment

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
-export function questionPopularity(questions) {
-  var question = questions;
+export function questionPopularity(params) {
+  //console.log(question);
+  var question = params[0];
+  console.log(question.get('answers').get('length'));
 
-  if (question.get('answers').get('length') >= 2) {
 
-    console.log("check these answers");
-  }
+
 }
 
 export default Ember.Helper.helper(questionPopularity);
