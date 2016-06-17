70134408a39766720c149a225096e633c373dd9d
alexhagge128 <alexhagge128@gmail.com>
2016-06-17 13:20:38 -0700

    implemented ember service

--- app/components/favorite-display.js
+++ app/components/favorite-display.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoriteQuestions: Ember.inject.service(),
-  
 
+
+  actions: {
+    removeFromFavs(item) {
+      this.get('favoriteQuestions').remove(item);
+    }
+  }
 });
--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -4,8 +4,7 @@ export function questionPopularity(params) {
   var question = params[0];
 
   if(question.get('answers').get('length') > 5) {
-    console.log("this topic is on fire");
-    //actions
+    
   }
   return ("Responses: " + question.get('answers').get('length'));
 }
